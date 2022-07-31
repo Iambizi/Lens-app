d511d06bf575ccd819af1392dcd13d626cacebb3
Iambizi <amirbzmn@gmail.com>
2022-07-31 13:16:07 -0400

    Fixes typo in const containing queries

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
-import { client, recommendedProfiles} from '../api'
+import { client, recommendProfiles} from '../api'
 
 export default function Home() {
+
   useEffect(()=>{
     fetchProfiles()
   },[])
 
   async function fetchProfiles(){
     try{
-      const response = await client.query(recommendedProfiles).toPromise();
-    console.log({response})
+      const response = await client.query(recommendProfiles).toPromise();
+      console.log({response});
     } catch(err){
-      console.log({err})
+      console.log({err});
     }
   }
+
   return (
     <div>
       
